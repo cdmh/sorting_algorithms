e9ed3e2990b22b195f7838343fa420e495a01413
Craig Henderson <cdm.henderson@gmail.com>
2013-09-18 09:14:04 +0100

    Added sort stability demonstration

--- sorting.cpp
+++ sorting.cpp
@@ -97,10 +97,7 @@ void test_sorts(C container, Pred pred=Pred())
     // the heap sort currently uses std::make_heap and std::sort_heap which both require
     // a random access iterator, so only vector is tested here
     std::cout << "Heap Sort " << container.size() << " elements\n";
-    run_container_inplace_sort(
-        cdmh::heap_sort<std::vector<std::string>, std::less<std::string>, true>,
-        std::vector<std::string>{ "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"},
-        std::less<std::string>());
+    run_container_inplace_sort(cdmh::heap_sort<C, Pred, true>, container, pred);
 
     std::cout << "Merge Sort " << container.size() << " elements\n";
     run_container_to_container_sort(cdmh::merge_sort<std::vector<typename C::value_type>, std::vector<typename C::value_type>, Pred, true>, std::vector<typename C::value_type>(container.begin(), container.end()), pred);
@@ -140,13 +137,20 @@ void test_all_sorts(C container)
 
 int main()
 {
-    test_all_sorts(std::vector<std::string>{ "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"});
-    test_all_sorts(std::vector<std::string>{ "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"});
+    test_all_sorts(std::vector<std::string>{ "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" });
+    test_all_sorts(std::vector<std::string>{ "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" });
     test_all_sorts(std::vector<int>{});
     test_all_sorts(std::vector<int>{ 3 });
     test_all_sorts(std::vector<int>{ 3, 4, 5, 9, 8, 2, 1, 7, 6 });
     test_all_sorts(std::vector<int>{ 60, 10, 410, 20, 40, 50, 60, 10, 40, 30, 40, 50, 60, 10, 40, 50, 6 });
-    test_all_sorts(std::vector<int>{ 10, 20, 30, 40, 510, 20, 50, 60, 10, 60, 130, 10, 20, 30, 40, 50, 60, 140, 10, 20, 30, 40, 50, 60, 1});
+    test_all_sorts(std::vector<int>{ 10, 20, 30, 40, 510, 20, 50, 60, 10, 60, 130, 10, 20, 30, 40, 50, 60, 140, 10, 20, 30, 40, 50, 60, 1 });
+
+    // shows stability of the sort by comparing doubles as ints
+    test_sorts(
+        std::vector<double>{ 1.2, 1.1, 0.4, 0.1, 0.9, 3.1, 3.6, 9.4, 9.8, 9.6, 3.2 },
+        [](double const &first, double const &second) {
+            return int(first)<int(second);
+        });
 
     std::vector<int> v{ 10, 20, 30, 40, 510, 20, 50, 60, 10, 60, 130, 10, 20, 30, 40, 50, 60, 140, 10, 20, 30, 40, 50, 60, 1};
     std::sort(v.begin(), v.end());
